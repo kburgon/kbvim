e9214b23d9a14fcb18e2db77609163d2f9adb827
Kevin Burgon <buburgo@gmail.com>
2026-06-27 06:41:05 -0600

    Set keymaps for fugitive

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,1 +1,20 @@
-return 'tpope/vim-fugitive'
+local M = {
+	'tpope/vim-fugitive',
+	lazy = false,
+}
+
+local function nkset(shortcut, cmd, opts)
+	vim.keymap.set('n', shortcut, cmd, opts)
+end
+
+function M.config()
+	print('setting fugitive keymaps')
+	nkset('<leader>gg', '<cmd>G<cr>')
+	nkset('<leader>gpl', '<cmd>G pull<cr>')
+	nkset('<leader>gps', function ()
+		require('git_actions_config').PushToOrigin()
+	end, { desc = "Push changes to the remote repository" })
+	nkset('<leader>gfl', '<cmd>G log -p -- %<cr>')
+end
+
+return M
